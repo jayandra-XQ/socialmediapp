087c682738032d4a5a38da7855f974353278086f
jayandra <jayandra@jayandra.jroot>
2023-05-21 00:02:15 +0530

    create and delete

--- main.js
+++ main.js
@@ -1,65 +1,65 @@
+/**
+ * ! selecting id element from inner.html
+ */
 let form = document.getElementById("form");
 let input = document.getElementById("input");
 let msg = document.getElementById("msg");
 let posts = document.getElementById("posts");
 
+
 /**
- * ! Buton Click -> Submit
+ * ! making button click submit
  */
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    console.log("button clicked")
-    formValidation();
-
+  e.preventDefault();
+  console.log("button clicked");
+  formValidation();
 });
 
 /**
- * ! form validation function
+ * ! form validation
  */
 let formValidation = () => {
-    if (input.value === "") {
-        msg.innerHTML = "Post cannot be blank";
-        console.log("failure")
-    }
-    else {
-        console.log("success");
-        msg.innerHTML = "";
-        acceptData();
-    }
+  if (input.value === "") {
+    msg.innerHTML = "Post cannot be blank";
+    console.log("failure");
+  } else {
+    console.log("successs");
+    msg.innerHTML = "";
+    acceptData();
+  }
 };
 
 /**
- * ! data is accepted
+ * ! data accept and upload on screen 
  */
 let data = {};
 
 let acceptData = () => {
-    data["text"] = input.value;
-    console.log(data);
-    createPost();
-
-}
-
-/**
- * ! upload on screen
- */
-let createPost = () => {
-    posts.innerHTML +=
-        `
-    <div>
-        <p>${data.text}</p>
-        <span class="options">
-            <i class="fa-solid fa-pen-to-square"></i>
-            <i onClick="deletePost(this)" class="fa-solid fa-trash"></i>
-        </span>
-    </div>
-    `;
-    input.value =  "";
+  data["text"] = input.value;
+  console.log(data);
+  createPost();
 };
 
-/**
- * ! Delete post
- */
+let createPost = () => {
+  posts.innerHTML += `
+  <div>
+    <p>${data.text}</p>
+    <span class="options">
+      <i onClick="editPost(this)" class="fas fa-edit"></i>
+      <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+    </span>
+  </div>
+  `;
+  input.value = "";
+};
+
 let deletePost = (e) => {
-    e.parentElement.parentElement.remove();
-} 
+  e.parentElement.parentElement.remove();
+};
+
+let editPost = (e) => {
+  input.value = e.parentElement.previousElementSibling.innerHTML;
+  e.parentElement.parentElement.remove();
+};
+
