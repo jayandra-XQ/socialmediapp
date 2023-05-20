ad53d3a27210c9bfa7c5517fd3d412ff6141a994
jayandra <jayandra@jayandra.jroot>
2023-05-20 23:48:28 +0530

    delete Post

--- main.js
+++ main.js
@@ -6,38 +6,60 @@ let posts = document.getElementById("posts");
 /**
  * ! Buton Click -> Submit
  */
-form.addEventListener("submit", (e) =>{
+form.addEventListener("submit", (e) => {
     e.preventDefault();
     console.log("button clicked")
     formValidation();
-    
+
 });
 
 /**
  * ! form validation function
  */
 let formValidation = () => {
-    if(input.value === ""){
+    if (input.value === "") {
         msg.innerHTML = "Post cannot be blank";
         console.log("failure")
     }
-    else{
+    else {
         console.log("success");
         msg.innerHTML = "";
         acceptData();
     }
 };
 
+/**
+ * ! data is accepted
+ */
 let data = {};
 
 let acceptData = () => {
     data["text"] = input.value;
     console.log(data);
     createPost();
-    
+
 }
 
-
+/**
+ * ! upload on screen
+ */
 let createPost = () => {
-    posts.innerHTML = data.text;
+    posts.innerHTML +=
+        `
+    <div>
+        <p>${data.text}</p>
+        <span class="options">
+            <i class="fa-solid fa-pen-to-square"></i>
+            <i onClick="deletePost(this)" class="fa-solid fa-trash"></i>
+        </span>
+    </div>
+    `;
+    input.value =  "";
 };
+
+/**
+ * ! Delete post
+ */
+let deletePost = (e) => {
+    e.parentElement.parentElement.remove();
+} 
